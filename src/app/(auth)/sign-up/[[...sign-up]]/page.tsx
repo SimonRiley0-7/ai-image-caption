003b27deeb904ef5fae364e0c737699ba3fc7e2b
Devastation <shivamwadatkar16@gmail.com>
2025-03-02 14:06:40 +0530

    first commit

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,13 @@
+import { SignUp } from "@clerk/nextjs";
+export default function SignUpPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <SignUp appearance={{ 
+        elements: { 
+          formButtonPrimary: 'bg-primary text-primary-foreground hover:bg-primary/90',
+          card: 'shadow-md'
+        }
+      }} />
+    </div>
+  );
+};
